d88a08c6ee18ff83a7d0a1d2cdc7672c1c5218c4
KaracaSuleyman <suleymankaraca399@gmail.com>
2026-02-15 22:50:08 +0000

    sad

---
+++ lessons/pip.py
@@ -0,0 +1,18 @@
+
+
+#pip python da apple store gibi bir paket yöneticisidir. pip ile python paketlerini kolayca yükleyebiliriz.
+# pip ile yüklenen paketler python'un standart kütüphanelerine eklenir ve python kodlarımızda bu paketleri kullanabiliriz.
+
+# pip ile paket yüklemek için terminale şu komutu yazabiliriz:
+# pip install paket_adı
+# Örneğin, numpy paketini yüklemek için terminale şu komutu yazabiliriz:
+# pip install numpy
+# pip ile yüklenen paketler python'un standart kütüphanelerine eklenir ve python kodlarımızda bu paketleri kullanabiliriz.
+# Örneğin, numpy paketini yükledikten sonra python kodumuzda şu şekilde kullanabiliriz:
+# import numpy as np
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr)
+
+import numpy as np
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
