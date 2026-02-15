8d4656ab1d8f4214cede2e98d38f397f58b7a3bf
KaracaSuleyman <suleymankaraca399@gmail.com>
2026-02-15 23:06:46 +0000

    234

--- lessons/virtual_enveriment.py
+++ lessons/virtual_enveriment.py
@@ -4,3 +4,17 @@
 # Virtual  Environments
 # Virtual Environments are a tool to keep the dependencies required by different projects in separate places, by creating virtual Python environments for them.
 # This is one of the most important tools that most of the Python developers use.
+# Why do we need Virtual Environments?
+# 1. To avoid version conflicts between packages.
+# 2. To manage dependencies for different projects.
+# 3. To create a clean environment for testing and development.
+# 4. To avoid installing packages globally which can affect other projects.
+# How to create a Virtual Environment?
+# 1. Using venv module (Python 3.3 and above)
+# 2. Using virtualenv (for older versions of Python)
+# 3. Using conda (for data science projects)
+# Using venv module
+# To create a virtual environment using the venv module, you can use the following command:
+# python -m venv myenv
+# This will create a virtual environment named "myenv" in the current directory.
+# To activate the virtual environment, you can use the following command:
