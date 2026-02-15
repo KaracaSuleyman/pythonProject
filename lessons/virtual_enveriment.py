94fef7d0862f5cf6cff332e602640cf36f1b1567
KaracaSuleyman <suleymankaraca399@gmail.com>
2026-02-15 22:59:14 +0000

    2

---
+++ lessons/virtual_enveriment.py
@@ -0,0 +1,6 @@
+
+
+
+# Virtual  Environments
+# Virtual Environments are a tool to keep the dependencies required by different projects in separate places, by creating virtual Python environments for them.
+# This is one of the most important tools that most of the Python developers use.
